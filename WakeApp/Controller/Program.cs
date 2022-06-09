ed309b9f9eef9c32e23479bb8329022a9885b0d4
SiiNeX Leqend <etulosinsky@allinsight.de>
2022-06-09 16:09:10 +0200

    better structure

--- WakeApp/Controller/Program.cs
+++ WakeApp/Controller/Program.cs
@@ -3,9 +3,9 @@
     using System;
     using System.IO;
     using System.Reflection;
-    using System.Runtime.CompilerServices;
     using System.Xml;
     using System.Xml.Linq;
+
     using WakeApp.Type;
 
     public sealed class Program
